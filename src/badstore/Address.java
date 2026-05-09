3e305ef319bcd37927f701c637f5284de17466bf
Erick Somentac <somentac.erick@gmail.com>
2026-05-09 17:11:30 -0500

    Fixing address

--- src/badstore/Address.java
+++ src/badstore/Address.java
@@ -2,8 +2,21 @@ package badstore;
 
 public class Address {
 
-test test
+    //Fields need to be private to protect obj data from changing
+    // addresses should not change after creation so we use final
+
+    private final String street;
+    private final String city;
+    private final String state;
+    private final String zip;
+    private final String country;
+
+    //constructor to enforce invariants, prevents Address obj from being created
+    //into an invalid state
+    public Address(String street, String city, String state, String zip, String country {
         
+    })
+
     @Override
     public String toString() {
         return street + ", " + city + ", " + state + " " + zip +
